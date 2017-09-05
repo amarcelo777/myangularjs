c781578313c542c92a07a8e6775e0f1f617829c7
amarcelo777 <31530152+amarcelo777@users.noreply.github.com>
2017-09-05 14:39:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@
 	angular.module('DIApp', [])
 	.controller('DIController', DIControllerFnc);
 	
-	function DIControllerFnc ($scope, $filter) {
+	function DIControllerFnc ($scope, $filter, $Injector) {
 			$scope.name = "Andy";
 			$scope.upper = function () {
 				var upcase = $filter('uppercase');
 				$scope.name = upcase($scope.name);
 			};
+			console.log($injector.annotate(DIControllerFnc));
 	}
 	
 	console.log(DIControllerFnc.toString());
